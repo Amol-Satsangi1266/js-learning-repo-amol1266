b5078ddb1a3c99a95cbc33d9ac8a0ad8f0af70aa
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-24 21:33:08 +0530

    Mastering Datatype Conversion

---
+++ Initial Learning/datatypeconversions.js
@@ -0,0 +1,17 @@
+// Just covering the basic datatype conversions first to build the understanding
+
+let studentScore = 100;
+console.log(studentScore);
+
+// String -> Its the most easy and understandable conversion
+
+let stringstudentScore = String(studentScore);
+console.log(stringstudentScore);
+
+//Boolean -> okayish
+let BooleanstudentScore = Boolean(stringstudentScore);
+console.log(BooleanstudentScore);
+
+//Number
+let againnumberstudentScore = Number(BooleanstudentScore);
+console.log(againnumberstudentScore);
