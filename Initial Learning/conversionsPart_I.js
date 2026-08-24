e118cc0b5269d51a0ee8e2c000a3687107832784
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-25 00:24:17 +0530

    Null values Discussion

---
+++ Initial Learning/conversionsPart_I.js
@@ -0,0 +1,8 @@
+let a = null;
+
+let b = Number(a);
+console.log(b)
+
+// This shows that when null is converted into Number it is made as ZERO as a output value
+
+// !----------------------A MORE ON DATATYPES---------------------!
