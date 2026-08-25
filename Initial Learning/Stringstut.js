f9ee58c1d6cfaceb03e98f537223ec106218a252
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-26 01:18:24 +0530

    String Literals

---
+++ Initial Learning/Stringstut.js
@@ -0,0 +1,19 @@
+let basicString = "Hello I am a String";
+
+//String Interpolation (spelling idk ;) )
+
+let name1 = "Amol";
+console.log(`Hello my name is ${name1}`);
+
+// String Methods Most Common and used
+
+let primeString = new String("Amol-Satsangi");
+
+console.log(primeString.slice(0,4));
+
+console.log(primeString.indexOf("S"));
+
+console.log(primeString.toUpperCase());
+
+console.log(primeString.replace("Amol","Great"));
+
