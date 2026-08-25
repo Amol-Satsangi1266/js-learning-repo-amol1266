b63e20e8cb33e91aa227504c77cc0af15a94f881
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-25 17:53:47 +0530

    New Datatypes Inspection

--- Initial Learning/conversionsPart_I.js
+++ Initial Learning/conversionsPart_I.js
@@ -6,3 +6,26 @@ console.log(b)
 // This shows that when null is converted into Number it is made as ZERO as a output value
 
 // !----------------------A MORE ON DATATYPES---------------------!
+
+// Doing a exercise to get the datatype of all
+
+let me = "Hi";
+console.log(typeof me);
+
+let me2 = Number(me);
+console.log(typeof me2);
+
+let me3 = Boolean(me);
+console.log(typeof me3);
+
+let me4 = Symbol(me);
+console.log(typeof me4);
+
+let me5 = Object(me);
+console.log(typeof me5);
+
+let me6 = null;
+console.log(typeof me6);
+
+
+
