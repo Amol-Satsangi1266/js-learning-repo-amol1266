601d8830f988b946e67bb8072d002c68a697b895
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-26 13:05:07 +0530

    Numbers and Mathematics in Javascript

---
+++ Initial Learning/numbers.js
@@ -0,0 +1,38 @@
+//Numbers in Javascript
+const balance = new Number(25);
+
+console.log(balance.toFixed(2));
+
+
+
+
+const amount1 = 24.9829;
+
+console.log(amount1.toPrecision(2));
+
+
+
+const amount2 = 10000000;
+console.log(amount2.toLocaleString('en-IN'));
+
+
+
+// Mathematics in JS
+
+
+
+console.log(Math);
+console.log(Math.abs(-2.98))
+
+console.log(Math.round(2.7685))
+
+console.log(Math.round((Math.random() * 10 + 1)))
+
+// A formula to get the ultimate value
+
+let min = 20
+let max = 40
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
