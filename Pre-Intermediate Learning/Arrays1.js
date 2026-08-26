8aadb4164124a6818415241c3850bc0aa0e69dc4
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-27 00:56:42 +0530

    Advanced Features on Arrays

--- Pre-Intermediate Learning/Arrays1.js
+++ Pre-Intermediate Learning/Arrays1.js
@@ -15,3 +15,24 @@ let numbers = [1, 2, 3, 4];
 let doubled = numbers.map(num => num * 2);      // [2, 4, 6, 8]
 let evens = numbers.filter(num => num % 2 === 0); // [2, 4]
 let sum = numbers.reduce((acc, curr) => acc + curr, 0); // 10
+
+
+// Features
+
+//spreading
+let marvel = ["spiderman","Dr. Strang", "Ghost Rider"]
+let dc = ["Red Hood","Constantine","John Stwart"]
+const heroes = [...marvel,...dc];
+
+console.log(heroes);
+
+
+//Flatnening
+
+const real_arr = [1,2,3,4,[5,6],7,[8,[9,10,[11,12]]]];
+
+const main_arr = real_arr.flat(Infinity);
+
+console.log(main_arr);
+
+
