a5aab0f8a914f1fe75aa36acd80f86c1e893e97e
Amol Satsangi <amolpankaj14@gmail.com>
2026-08-27 16:26:24 +0530

    Object Chaining

---
+++ Pre-Intermediate Learning/2_Objects.js
@@ -0,0 +1,28 @@
+// Object Chaining
+
+const obj = 
+{
+    name: 
+    {
+        firstName: "Amol",
+        lastName: "Satsangi"
+    },
+
+    email:
+    {
+        workMail: "amolpankaj14@gmail.com",
+        homeMail: "amolsatsangi05@gmail.com"
+    },
+
+    phone:
+    {
+        workphone: 0o0,
+        homephone: 0o0
+    }
+
+}
+
+console.log(obj.name.firstName);
+console.log(obj.email.workMail);
+console.log(obj.phone.workphone);
+console.log(obj.name.lastName);
